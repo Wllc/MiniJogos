67022e0d0d1449d81e342baba8b56e1045a858cb
Wllc <50117208+Wllc@users.noreply.github.com>
2019-06-16 13:01:55 -0300

    Add files via upload

---
+++ hk/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1560700810,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/shootbotton-sheet0.png",
+		"images/shootbotton-sheet1.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/arrowupbutton-sheet0.png",
+		"images/arrowupbutton-sheet1.png",
+		"images/background-sheet0.png",
+		"images/background-sheet1.png",
+		"images/enemycontroller-sheet0.png",
+		"images/enemycontroller-sheet1.png",
+		"images/bottomdestroyer-sheet0.png",
+		"images/energyframeui-sheet0.png",
+		"images/energyui-sheet0.png",
+		"images/powerupcontroller-sheet0.png",
+		"images/powerupcontroller-sheet1.png",
+		"images/parede-sheet0.png",
+		"images/powerupcontroller2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
